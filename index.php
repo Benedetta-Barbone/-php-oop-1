181cde943122c04bbe62511d189530b10fd8e9c5
Benedetta-Barbone <barbone1912@gmail.com>
2024-07-02 16:35:42 +0200

    definito costruttore

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- create un file index.php in cui:
 è definita una classe ‘Movie’
-=> all'interno della classe sono dichiarate delle variabili d'istanza
+
 => all'interno della classe è definito un costruttore
 => all'interno della classe è definito almeno un metodo
 vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
@@ -10,6 +10,12 @@ class Movie{
     public $titolo;
     public $genere;
     public $anno;
+
+    function __construct($titolo, $genere, $anno){
+        $this->titolo = $titolo;
+        $this->genere = $genere;
+        $this->anno = $anno;
+    }
 }
 
 ?>
