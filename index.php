79e82b20fddd03cd77edb08ff93773059d1356f2
Benedetta-Barbone <barbone1912@gmail.com>
2024-07-02 17:06:14 +0200

    definito un metodo e istanziati oggetti Movie

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!-- create un file index.php in cui:
-è definita una classe ‘Movie’
-
-=> all'interno della classe è definito un costruttore
-=> all'interno della classe è definito almeno un metodo
-vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
 <?php
 
 class Movie{
@@ -16,6 +10,44 @@ class Movie{
         $this->genere = $genere;
         $this->anno = $anno;
     }
+
+    function getDescription(){
+        return "Titolo: $this->titolo, Genere: $this->genere, Anno: $this->anno,";
+    }
 }
 
+$movie1 = new Movie("La vita è bella", "Comedy/Drama", 1997);
+$movie2 = new Movie("Nuovo Cinema Paradiso", "Drama", 1988);
+$movie3 = new Movie("Il postino","Romance/Drama",1994);
+$movie4 = new Movie("La dolce vita", "Drama", 1960);
+$movie5 = new Movie("Ladri di biciclette","Drama", 1948 );
+
+
+$movies = [$movie1, $movie2, $movie3, $movie4, $movie5];
+
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 1</title>
+</head>
+<body>
+    <header>
+        <h1>
+            Lista Film
+        </h1>
+    </header>
+    <main>
+    <section>
+        <?php foreach ($movies as $movie): ?>
+            <p><?php echo ($movie->getDescription()); ?></p>
+        <?php endforeach; ?>
+    </section>
+    </main>
+</body>
+</html>
